49136aeda91247469d0a7bf0a96ea64698248619
BertaFernandezGomez <bertafg80@educastur.es>
2025-11-05 10:45:06 +0100

    Add files via upload

---
+++ ejercicios_p5js/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(400, 400);
+  background(245, 239, 137);
+  line(50, 50, 50, 350);
+  line(50, 50, 250, 50);
+  line(50, 200, 200, 200);
+}
---
+++ ejercicios_p5js/sketch1.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+  background(169, 196, 179);       
+  
+  stroke(181, 145, 174);
+  strokeWeight(3);
+  line(100, 100, 300, 100);
+
+  stroke(191, 209, 153); 
+  strokeWeight(6);        
+  line(300, 100, 300, 300);
+
+  stroke(153, 200, 209);
+  strokeWeight(2);  
+  line(300, 300, 100, 300);
+
+  stroke(247, 250, 150); 
+  strokeWeight(4); 
+  line(100, 300, 100, 100);
+}
